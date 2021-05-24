67c3b1fe47643a469b801dc3307149321d8f7fc0
Jueun Park <juepark@c5r2s5.42seoul.kr>
2021-05-24 19:06:26 +0900

    [added mutex directory]

---
+++ philo01_func/mutex/mutex.c
@@ -0,0 +1,57 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+pthread_mutex_t	mutex;
+int				g_number;
+
+void	print_index_loop(char *str, int max)
+{
+	int i;
+
+	i = 0;
+	while (i < max)
+	{
+//		pthread_mutex_lock(&mutex);
+		g_number++;
+		printf("[%s] %d\n", str, g_number);
+		usleep(1000 * 100);
+		++i;
+//		pthread_mutex_unlock(&mutex);
+	}
+}
+
+void	*thread_routine(void *arg)
+{
+	print_index_loop((char *)arg , 5);
+	return (NULL);
+}
+
+int main()
+{
+	pthread_t	tid;
+	void		*t_res;
+	int			state;
+	int			create_res;
+	int			join_res;
+
+	//pthread_mutex_init -- create a mutex
+	//		args:	1. mutex()
+	//				2. attr(속성) if it's NULL --> default attributes are used.
+	//				success: 0, fail: errno
+
+	g_number = 0;
+	state = pthread_mutex_init(&mutex, NULL);
+	if (state)
+	{
+		printf("fail to init mutex\n");
+		exit(1);
+	}
+	create_res = pthread_create(&tid, NULL, thread_routine, (void *)"thread");
+	if (create_res != 0)
+		return (1);
+	thread_routine((void *)"main");
+	join_res = pthread_join(tid, NULL);
+	printf("join result: %d\n", join_res);
+}
---
+++ philo01_func/pthread/pthread.c
@@ -0,0 +1,57 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/errno.h>
+
+void	print_index_loop(char *str, int max)
+{
+	int i;
+
+	i = 0;
+	while (i < max)
+	{
+		printf("[%s]i is %d\n", str, i);
+		usleep(1000 * 100);
+		++i;
+	}
+}
+
+void	*thread_routine(void *arg)
+{
+	print_index_loop("thread", 5);
+	return (NULL);
+}
+
+int 	main()
+{
+	pthread_t	tid;
+	int			create_res;
+	int			detach_res;
+	int			join_res;
+
+	// [ 1. create new thread in this process ]
+	// args:	1.thread(ID)
+	//			2.attr(info of new thread, generally filled with NULL)
+	//			3.function ptr
+	//			4.function's argument
+	//			success -> return: 0	// fail -> return: errno
+	create_res = pthread_create(&tid, NULL, thread_routine, NULL);
+	if (create_res != 0)
+		return (1);
+	print_index_loop("main", 3);
+
+	// [ 2. detach thread from main thread ]
+	// 			success -> return: 0	// fail -> return: errno
+	detach_res = pthread_detach(tid);
+	printf("detaced result: %d\n", detach_res);
+
+
+	// [ 3. (join)wait for thread ]
+	// args:	1. thread(ID)
+	//			2.thread function's return value
+	join_res = pthread_join(tid, NULL);
+	printf("join result: %d\n", join_res);
+	return (0);
+}
